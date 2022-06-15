0b71cbd30f852341293dc2d9306ebf6c75e7a0e7
AlexanderStreltsov <alexander.streltsov91@yandex.ru>
2022-06-15 06:30:15 +0300

    feat: edit constructor styles

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -2,6 +2,7 @@ import {
   ConstructorElement,
   Button,
   CurrencyIcon,
+  DragIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
 import { ingredientPropType } from "../../utils/prop-types";
@@ -9,13 +10,18 @@ import constructorStyles from "./burger-constructor.module.css";
 import { createRandomKey } from "../../utils/create-random-key";
 
 const BurgerConstructor = ({ ingredients, handleCreateOrder }) => {
-  const ingredient = ingredients.find(
+  const ingredientBun = ingredients.find(
     (ingredient) => ingredient.type === "bun"
   );
-  const sum = ingredients.reduce(
-    (acc, ingredient) => ingredient.price + acc,
-    0
+  const ingrdientsWithoutBun = ingredients.filter(
+    (ingredient) => ingredient.type !== "bun"
   );
+  const sum =
+    ingrdientsWithoutBun.reduce(
+      (acc, ingredient) => ingredient.price + acc,
+      0
+    ) +
+    ingredientBun.price * 2;
 
   return (
     <section className={constructorStyles.section}>
@@ -23,13 +29,16 @@ const BurgerConstructor = ({ ingredients, handleCreateOrder }) => {
         <ConstructorElement
           type="top"
           isLocked={true}
-          text={ingredient.name}
-          price={ingredient.price}
-          thumbnail={ingredient.image}
+          text={`${ingredientBun.name} (верх)`}
+          price={ingredientBun.price}
+          thumbnail={ingredientBun.image}
         />
         <ul className={constructorStyles.list}>
-          {ingredients.map((item) => (
+          {ingrdientsWithoutBun.map((item) => (
             <li key={createRandomKey()} className={constructorStyles.listItem}>
+              <i key={createRandomKey()} className={constructorStyles.dragIcon}>
+                <DragIcon key={createRandomKey()} />
+              </i>
               <ConstructorElement
                 key={item._id}
                 text={item.name}
@@ -42,9 +51,9 @@ const BurgerConstructor = ({ ingredients, handleCreateOrder }) => {
         <ConstructorElement
           type="bottom"
           isLocked={true}
-          text={ingredient.name}
-          price={ingredient.price}
-          thumbnail={ingredient.image}
+          text={`${ingredientBun.name} (низ)`}
+          price={ingredientBun.price}
+          thumbnail={ingredientBun.image}
         />
       </div>
       <div className={`${constructorStyles.orderWrapper} mt-10`}>
