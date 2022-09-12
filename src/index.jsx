b0d3d19deaa46da38d28538e46367deede14b240
AlexanderStreltsov <alexander.streltsov91@yandex.ru>
2022-09-12 17:45:06 +0300

    feat: fix 404 for gh pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./components/app/app";
 import { Provider } from "react-redux";
 import store from "./services/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL + "/"}>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
